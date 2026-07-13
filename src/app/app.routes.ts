e8751ac468658d2768082c3ac30b45996a9154dc
LCrowe42 <LNCrowe@outlook.com>
2026-07-13 02:22:03 -0400

    confirmation page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './home/home';
 import { ContactForm } from './contact-form/contact-form';
 import { Projects } from './projects/projects';
 import { About } from './about/about';
+import { Confirmation } from './confirmation/confirmation';
 
 export const routes: Routes = [
   { path: '', component: Home, title: 'Place Request' },
   { path: 'contact', component: ContactForm, title: 'Contact'},
   { path: 'projects', component: Projects, title: 'Projects'},
-  { path: 'about', component: About, title: 'About' }
+  { path: 'about', component: About, title: 'About' },
+  { path: 'confirmation', component: Confirmation, title: 'Confirmation' }
 ];
